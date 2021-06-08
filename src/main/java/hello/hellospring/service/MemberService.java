8093d5caf5457d22cc8413d66caf13786fd13eaa
bluedog129 <bluedog129@gmail.com>
2021-06-08 22:47:01 +0900

    회원서비스 테스트

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,8 +9,12 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     //회원가입
     public Long join(Member member) {
         // 같은 이름이 있는 죽복 회원 X
